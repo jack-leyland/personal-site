5e7d998b098db3f740067af2b8271d574c464d6a
jack-leyland <jackjleyland@gmail.com>
2022-10-18 09:48:20 +0100

    add resume download

--- src/components/ContactPanel.vue
+++ src/components/ContactPanel.vue
@@ -2,13 +2,17 @@
 import GithubLogo from './icons/GithubLogo.vue';
 import LinkedinLogo from './icons/LinkedinLogo.vue';
 
+const resumeLink = "/resume.pdf"
+
 </script>
 
 
 <template>
-    <button
+    <a :href="resumeLink" download="jack_leyland_resume.pdf">
+        <button
         class="font-title text-black text-sm sm:text-lg mt-8 sm:mt-16 mb-2 p-3 mr-4 bg-greenish hover:text-white hover:bg-black cursor-pointer transition-all duration-200">Download
         my resume</button>
+    </a>
     <span class="font-title text-black block sm:inline text-sm sm:text-lg"> or, contact: <a href="mailto:jack@leyland.dev" target="_blank" rel="noopener noreferrer"><span
             class="text-mainPurple font-bold">jack@leyland.dev</span></a></span>
     <div class="mb-2 mt-24 font-alt text-gray-500 tracking-wide test-md sm:text-lg font-light">Find me elsewhere</div>
